f1dd6cfb73b13f53ccb8ec886b493883fa6b0f92
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2021-12-06 18:59:55 +0100

    Menu tutorial end

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -6,12 +6,7 @@ const Question = ({ title, info }) => {
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button
-          className="btn"
-          onClick={() => {
-            setShowInfo(!showInfo);
-          }}
-        >
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
