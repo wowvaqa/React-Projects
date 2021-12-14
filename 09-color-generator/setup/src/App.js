c30ba805fa97dc9d3ba18278f5c842cc21e4924e
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2021-12-14 19:08:57 +0100

    Color generator project tutorial end

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,67 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [count, setCount] = useState(10);
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    try {
+      let colors = new Values(color).all(count);
+      setList(colors);
+      setCount(count);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  // w input klasa CSS error jest wywoływana w momencie jeżeli useState error = TRUE
+  return (
+    <>
+      <section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={parseInt(count)}
+            onChange={(event) => setCount(parseInt(event.target.value))}
+            placeholder="10"
+          ></input>
+          <input
+            type="text"
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          ></input>
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          const hex = color.hex;
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
